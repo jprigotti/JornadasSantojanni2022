184eb841ea5fcfaed04d55a03f85f99bc88c9c26
Juan Pablo Rigotti <rigottijp@gmail.com>
2022-09-24 11:58:30 -0300

    countdown

---
+++ indexJS.js
@@ -0,0 +1,31 @@
+//Countdown code
+//Set the date we are counting down to
+
+
+
+var countDownDate = new Date("Nov 7, 2022, 09:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function(){
+    
+    //Get toda's
+    var now = new Date().getTime();
+    console.log(now);
+
+    //Find the distance between now and the countDown date
+    var distance = countDownDate - now;
+
+    //Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance/(1000*60*60*24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+   console.log(days.toString());
+
+    $('input[name=days]').val(days.toString());
+    $('input[name=hours]').val(hours.toString());
+    $('input[name=minutes]').val(minutes);
+    $('input[name=seconds]').val(seconds);
+
+},1000);
