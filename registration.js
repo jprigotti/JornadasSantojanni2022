dd44a9550d055c12cb353c519c6fd119e49348b9
Juan Pablo Rigotti <rigottijp@gmail.com>
2022-09-28 01:38:14 -0300

    updated form style and validation

--- registration.js
+++ registration.js
@@ -1,17 +1,39 @@
+// CODIGO PARA GENERAR LOS OPTIONS DEL SELECT SERVICIOS
+$.ajax({
+    method: "get",
+    redirect: "follow",
+    url: 'https://script.google.com/macros/s/AKfycbzQeIrYwMdcPIgQb7wZ9kX5_4ztb5jUEESf9CnAnmYw1DRGWn3sXUYdJ4IZjLbJSTmnww/exec',
+    dataType: 'json',
+    accepts: 'application/json',
+    success: (list) => {
+        let i = 0;
+        let formSelect = document.getElementById("formSelect");
+        let option;
+
+        do {
+            // console.log(list[i][0]);
+            option = document.createElement('option');
+            // option.classList.add('optionClass');
+            option.value = list[i][0];
+            option.innerHTML = list[i][0];
+            formSelect.appendChild(option);
+            i++;
+        } while (list[i] != null);
+    },
+    error: (err) => console.log(err)
+});
 
 
 // CODIGO PARA EL SUBMIT DEL FORMULARIO DE INSCRIPCION
 $('form.ajax').submit(function (evento) {
 
     evento.preventDefault();  // avoid to execute the actual submit of the form.
-    processingRegistration(); //update the submit button to show progress
 
     //ADD JS to valdate input data PENDING - then read the form inputs
     let that = $(this),
         url = that.attr('action'),
         method = that.attr('method'),
         data = {}; //this is gonna be a JS object to store all input values
-
 
     //loop through all the "name" items in the Form to load the data object
 
@@ -25,6 +47,20 @@ $('form.ajax').submit(function (evento) {
     console.log(data);
     //check if user is registered or not, runnig a query using AJAX-GET passing data object
 
+    (validateInput(data)) ? insertUserSS(url, method, data) : console.log("complete los campos");
+    
+
+});
+
+
+
+// Local Functions
+
+// Esta funcion recibe un objecto data con todos los campos del usuario 
+// que envia a Google WebApp para insertarlo en el SS
+function insertUserSS(url, method, data) {
+    processingRegistration(); //update the submit button to show progress
+    //Primero validamos que el usuario no este previamente registrado
     $.ajax({
         method: "get",
         redirect: "follow",
@@ -57,10 +93,7 @@ $('form.ajax').submit(function (evento) {
         },
         error: (err) => console.log(err)
     });
-});
-
-
-// Local Functions
+}
 
 function processingRegistration() {
     // $("#formSubmit").val("Procesando...");
@@ -198,29 +231,116 @@ $('#alertContainerCloseRU').on('mousedown', function () {
 // formSelect.appendChild(option);
 
 
-$.ajax({
-    method: "get",
-    redirect: "follow",
-    url: 'https://script.google.com/macros/s/AKfycbzQeIrYwMdcPIgQb7wZ9kX5_4ztb5jUEESf9CnAnmYw1DRGWn3sXUYdJ4IZjLbJSTmnww/exec',
-    dataType: 'json',
-    accepts: 'application/json',
-    success: (list) => {
-        let i = 0;
-        let formSelect = document.getElementById("formSelect");
-        let option;
 
-        do {
-            // console.log(list[i][0]);
-            option = document.createElement('option');
-            // option.classList.add('optionClass');
-            option.value = list[i][0];
-            option.innerHTML = list[i][0];
-            formSelect.appendChild(option);
-            i++;
-        } while (list[i] != null);
-    },
-    error: (err) => console.log(err)
-});
+
+
+
+function validateInput(data) {
+    console.log(data);
+    var arrayCheck = [];
+    //Validamos el input FirstName
+    if (data.firstName == "") {
+        $('.enterFirstName').css({ 'visibility': 'visible' });
+        $('#inputFirstName').addClass('inputError');
+        arrayCheck.push(false);
+    } else {
+        $('.enterFirstName').css({ 'visibility': 'hidden' });
+        $('#inputFirstName').removeClass('inputError');
+        arrayCheck.push(true)
+    }
+
+    //validamos el input LastName
+    if (data.lastName == "") {
+        $('.enterLastName').css({ 'visibility': 'visible' });
+        $('#inputLastName').addClass('inputError');
+        arrayCheck.push(false);
+    } else {
+        $('.enterLastName').css({ 'visibility': 'hidden' });
+        $('#inputLastName').removeClass('inputError');
+        arrayCheck.push(true);
+    }
+
+    //validamos el input Cell
+    if (data.cell == "") {
+        $('.enterCell').css({ 'visibility': 'visible' });
+        $('#inputCell').addClass('inputError');
+        arrayCheck.push(false);
+    } else {
+        $('.enterCell').css({ 'visibility': 'hidden' });
+        $('#inputCell').removeClass('inputError');
+        arrayCheck.push(true);
+    }
+
+    //validamos el input Email
+    if (data.email == "") {
+        $('.enterEmail').css({ 'visibility': 'visible' });
+        $('#inputEmail').addClass('inputError');
+        arrayCheck.push(false);
+    } else {
+        $('.enterEmail').css({ 'visibility': 'hidden' });
+        $('#inputEmail').removeClass('inputError');
+        arrayCheck.push(true);
+    }
+
+    //validamos el input Servicio
+    if (data.servicio == "Elegir") {
+        $('.enterServicio').css({ 'visibility': 'visible' });
+        $('#formSelect').addClass('inputError');
+        arrayCheck.push(false);
+    } else {
+        $('.enterServicio').css({ 'visibility': 'hidden' });
+        $('#formSelect').removeClass('inputError');
+        arrayCheck.push(true);
+    }
+
+    console.log(arrayCheck);
+    if (arrayCheck.some((item) => item == false)) {
+        $('.warningMessage').css({ 'display': 'inline' });
+        return false;
+    } else {
+        $('.warningMessage').css({ 'display': 'none' });
+        return true;
+    }
+}
+
+
+$('#inputFirstName').on('keypress', function(e){
+    console.log("input first name changed");
+    $('.enterFirstName').css({ 'visibility': 'hidden' });
+    $('#inputFirstName').removeClass('inputError');
+    $('.warningMessage').css({ 'display': 'none' });
+})
+
+$('#inputLastName').on('keypress', function(e){
+    console.log("input first name changed");
+    $('.enterLastName').css({ 'visibility': 'hidden' });
+    $('#inputLastName').removeClass('inputError');
+    $('.warningMessage').css({ 'display': 'none' });
+})
+
+$('#inputCell').on('keypress', function(e){
+    console.log("input first name changed");
+    $('.enterCell').css({ 'visibility': 'hidden' });
+    $('#inputCell').removeClass('inputError');
+    $('.warningMessage').css({ 'display': 'none' });
+})
+
+$('#inputEmail').on('keypress', function(e){
+    console.log("input first name changed");
+    $('.enterEmail').css({ 'visibility': 'hidden' });
+    $('#inputEmail').removeClass('inputError');
+    $('.warningMessage').css({ 'display': 'none' });
+})
+
+$('#formSelect').on('change', function(e){
+    console.log("input first name changed");
+    $('.enterServicio').css({ 'visibility': 'hidden' });
+    $('#inputServicio').removeClass('inputError');
+    $('.warningMessage').css({ 'display': 'none' });
+})
+
+
+
 
 
 // let optionClass = document.querySelector('#formSelect');
